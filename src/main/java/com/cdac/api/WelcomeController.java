d76b876493ff22f5e584c4e745cd0afc5f1ab5cf
vinss1919 <109404613+vinss1919@users.noreply.github.com>
2022-08-06 13:21:27 +0530

    Update WelcomeController.java

--- src/main/java/com/cdac/api/WelcomeController.java
+++ src/main/java/com/cdac/api/WelcomeController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class WelcomeController {
+	int conunt; 
 	@GetMapping("/message")
 	public String getMessage() {
+		try{
+		}catch(expection e){
+		}
 		return "welcome...";
 	}
 }
